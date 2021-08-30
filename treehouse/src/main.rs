abb631df6ba31fa5bacb69c1c3b04eafd42ef5de
tamas <tamas.smahajcsikszabo@outlook.hu>
2021-08-30 22:08:57 +0200

    chapter2 finished

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -13,75 +13,114 @@ use std::io::stdin;
 // range not includes last element unless added like 0..=10
 //:? is the debug placeholder, a.k.a. raw printing
 //:#? is for pretty printing, it prints any type that support Debug trait
+//loops: they run code inside a block until *break* is encountered
 
+// enumerations: predefined set of values, can be any type even functions
+// can derive functionality, just like structs
+// e.g. Debug allows them to be print their values by name
+// like struct and implicit return declarations, doesn't end with colon
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
+}
+// the Debug trait lets any type to printed
+// it will allow println! with {:?} to print the entire struct
+// struct field values can be any, other sructs, too
+// i8 ~ 8 bit signed integer
 #[derive(Debug)]
 struct Visitor {
     name: String,
-    greeting: String,
+    action: VisitorAction,
+    age: i8,
 }
 
 impl Visitor {
-    fn new(name: &str, greeting: &str) -> Self {
+    fn new(name: &str, action : VisitorAction, age : i8) -> Self {
         // the lack of semicolon denotes "implicit return" syntax
         Self {
             name: name.to_lowercase(),
-            greeting: greeting.to_string(),
+            action,
+            age
         }
     }
 
+    //option2 is *destructuring* of 'note'
     fn greet_visitor(&self) {
-        println!("{}", self.greeting);
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the treehouse, {}!", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}!", self.name);
+                println!("{}", note);
+                if self.age<21 {
+                    println!("Do not serve alcohol to {}", self.name);
+                }
+            }
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
+        }
     }
 }
 
 fn what_is_your_name() -> String {
-    let mut visitor_name = String::new(); 
+    let mut visitor_name = String::new();
     stdin()
-        .read_line(&mut visitor_name) 
+        .read_line(&mut visitor_name)
         .expect("Failed to read line"); //function chaining
-    visitor_name
-        .trim()
-        .to_lowercase()
+    visitor_name.trim().to_lowercase()
 }
 
 fn main() {
-    println!("What's your name?");
-    let name = what_is_your_name();
-    // let visitor_list = ["bert", "steve", "fred"];
-    // the alternative syntax with type and size declarations, otherwise inferred by Rust
-    let visitor_list = [
-        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
-        Visitor::new("steve", "Hi Steve, Your mil is in the fridge."),
-        Visitor::new("fred", "Wow, who invited Fred?"),
-    ];
-    // println!("Hello {:?}", visitor_name);
-    // println!("Hello {}", name);
+    loop {
+        println!("What's your name? (Leave empty and press Enter to quit)");
+        let name = what_is_your_name();
+        // let visitor_list = ["bert", "steve", "fred"];
+        // the alternative syntax with type and size declarations, otherwise inferred by Rust
+        // using the vec! macro for Vectors
+        let mut visitor_list = vec![
+            Visitor::new("bert", VisitorAction::AcceptWithNote{note: String::from("Hello Bert, enjoy your treehouse.")}, 45),
+            Visitor::new("steve", VisitorAction::Accept, 19),
+            Visitor::new("fred", VisitorAction::Refuse, 11),
+        ];
+        // println!("Hello {:?}", visitor_name);
+        // println!("Hello {}", name);
 
-    // searching with an iterator
-    let known_visitor = visitor_list
-        .iter()
-        .find(|visitor| visitor.name == name);
+        // searching with an iterator
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
 
-    // find() returns an Option
-    // Options have either Some(x) or None values
-    //
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        // fat arrow => is for executing code for matches
-        None => println!("You are not on the visitor list. Please leave.")
+        // find() returns an Option
+        // Options have either Some(x) or None values
+        //
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            // fat arrow => is for executing code for matches
+            None => {
+                if name.is_empty() {
+                    //jumping to the end of the loop
+                    break;
+                } else {
+                    println!("{} is not on the visitor list. Please leave.", name);
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+                    println!("The final list of visitors:");
+                    println!("{:#?}", visitor_list);
+                }
+            }
+        }
+
+        // let mut allow_them_in = false;
+        // for visitor in &visitor_list {
+        // // it could have been indexed too, but direct iteration is safer and cleaner
+        //     if &visitor_name == visitor.name {
+        //         allow_them_in = true;
+        //     }
+        // }
+
+        // if allow_them_in {
+        //     println!("Welcome to the Treehouse, {}!", visitor_name);
+        // } else {
+        //     println!("Sorry, you aren't on the list!");
+        // }
     }
-
-    // let mut allow_them_in = false;
-    // for visitor in &visitor_list {
-    // // it could have been indexed too, but direct iteration is safer and cleaner
-    //     if &visitor_name == visitor.name {
-    //         allow_them_in = true;
-    //     }
-    // }
-
-    // if allow_them_in {
-    //     println!("Welcome to the Treehouse, {}!", visitor_name);
-    // } else {
-    //     println!("Sorry, you aren't on the list!");
-    // }
 }
