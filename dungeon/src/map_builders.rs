6cf6cd321ea1f48aeab69677c31f1f6e6b99b72f
tamas <tamas.smahajcsikszabo@outlook.hu>
2021-09-07 22:59:33 +0200

    scripts updated

--- dungeon/src/map_builders.rs
+++ dungeon/src/map_builders.rs
@@ -1,20 +1,18 @@
 use crate::prelude::*;
-const NUM_ROOMS: usize = 20;
+const NUM_ROOMS: usize = 30;
 
 //mapbuilder uses its own copy of the map and passes it to the game
 //room-carving algorithms
 pub struct MapBuilder {
-    pub map : Map,
-    pub rooms : Vec<Rect>,
-    pub player_start : Point,
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub player_start: Point,
 }
 
 impl MapBuilder {
     fn fill(&mut self, tile: TileType) {
-        self.map.tiles
-            .iter_mut()
-            .for_each(|t| *t = tile);
-        // *  is a de-reference: modification happens 
+        self.map.tiles.iter_mut().for_each(|t| *t = tile);
+        // *  is a de-reference: modification happens
         // on the referenced variable not the reference
     }
 
@@ -34,16 +32,71 @@ impl MapBuilder {
             }
             if !overlap {
                 room.for_each(|p| {
-                    if p.x > 0 && 
-                        p.x < SCREEN_WIDTH && 
-                            p.y > 0 && 
-                            p.y < SCREEN_HEIGHT {
-                                let ids = map_idx(p.x, p.y);
-                                self.map.tiles[idx] = TileType::Floor;
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
+                        let idx = map_idx(p.x, p.y);
+                        self.map.tiles[idx] = TileType::Floor;
                     }
                 });
                 self.rooms.push(room)
             }
         }
+    }
+
+    // to draw 'dog-leg' corridors
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        use std::cmp::{max, min};
+        // range iterator:
+        for y in min(y1, y2)..=max(y1, y2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        use std::cmp::{max, min};
+        // range iterator:
+        for x in min(x1, x2)..=max(x1, x2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
+        let mut rooms = self.rooms.clone();
+        // cmp() compares two elements and indicate if they are 
+        // the same or one is greater
+        rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x));
+
+        // enumerate counts items and returns in a tuple(counter, item)
+        // skip because previous would be otherwise invalid for the first room
+        // tuples are created with (..)
+        // can be destructured as let (a,b,c) = tuple; - turning them into named variables
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let prev = rooms[i-1].center();
+            let new = room.center();
+
+            if rng.range(0,2) == 1 {
+                self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
+                self.apply_vertical_tunnel(prev.y, new.y, new.x);
+            } else {
+                self.apply_vertical_tunnel(prev.y, new.y, prev.x);
+                self.apply_horizontal_tunnel(prev.x, new.x, new.y);
+            }
+        }
+    }
+
+    pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+        let mut mb = MapBuilder{
+            map: Map::new(),
+            rooms: Vec::new(),
+            player_start: Point::zero()
+        };
+        mb.fill(TileType::Wall);
+        mb.build_random_rooms(rng);
+        mb.build_corridors(rng);
+        mb.player_start = mb.rooms[0].center();
+        mb
     }
 }
