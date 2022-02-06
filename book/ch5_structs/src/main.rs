a417dd9ac44495c344b946b04eface0c99c9315c
Tamas@fedora <tsmahajcsiks@avaya.com>
2022-02-06 16:03:44 +0100

    struct updates

--- book/ch5_structs/src/main.rs
+++ book/ch5_structs/src/main.rs
@@ -3,6 +3,7 @@
 
 fn main() {
     let guard = Character{
+        // we use owned tpyes rather than references as no lifetimes are defined
         name: String::from("regular guard"),
         origin: String::from("Central"),
         level: 3,
@@ -10,6 +11,14 @@ fn main() {
         playable: false,
     };
     println!("Character name: {}", guard.name);
+
+    let guard2 = Character{
+        name: String::from("leader"),
+        // struct update syntax
+        ..guard
+    };
+    println!("Character name: {}", guard2.name);
+    let params=Stats(0,100,122);
 }
 
 struct Character {
@@ -20,13 +29,21 @@ struct Character {
     experience: i32
 }
 
-fn spawn_character(name: String, level: i16, experience: i32, origin: String) {
+fn spawn_character(name: String, level: i16, experience: i32, origin: String) -> Character {
     Character{
-        name=String::from(name),
+        name: String::from(name),
+        // field init shorthand
         level,
         experience,
-        origin=String::from(origin),
+        origin: String::from(origin),
         playable: false,
     }
-
 }
+
+// tuple struct examples
+// this creates a new type different of another struct tuple with the same elements
+struct Stats(i32, i32, i32);
+
+
+// unit-like structs
+struct Event();
